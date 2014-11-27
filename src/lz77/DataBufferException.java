ed927940d91085d5b248e50bae51336cf191e42d
Alexander Kirakozov <akirakozov@yandex-team.ru>
2014-11-27 15:37:42 +0400

    lz77 decode test using code from https://github.com/vhly/lz77-java

---
+++ src/lz77/DataBufferException.java
@@ -0,0 +1,13 @@
+package lz77;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vhly[FR]
+ * Date: 14-1-17
+ * Email: vhly@163.com
+ */
+public class DataBufferException extends Exception {
+    public DataBufferException(String message) {
+        super(message);
+    }
+}
---
+++ src/lz77/test/LZ77Test.java
@@ -0,0 +1,30 @@
+package lz77.test;
+
+import com.sun.xml.internal.ws.util.StreamUtils;
+import lz77.LZ77InputStream;
+import org.junit.Test;
+import sun.misc.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * @author akirakozov
+ */
+public class LZ77Test {
+
+    @Test
+    public void decode() throws IOException {
+        FileInputStream in = new FileInputStream(new File("test-lz77.txt"));
+        LZ77InputStream lz77 = new LZ77InputStream(in);
+
+        int len;
+        int bufferSize = 2048;
+        byte[] data = new byte[bufferSize];
+        while ((len = lz77.read(data, 0, bufferSize)) != -1) {
+            System.out.println(new String(data));
+        }
+    }
+}
